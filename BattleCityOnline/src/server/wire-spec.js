e69dc9a8854cd18bec36e4190f2e1df71c3c353c
Duong Nguyen <duongna@gmail.com>
2013-03-10 15:33:14 +0100

    Migrated dependencies from lib to amd_modules Added frozen

--- BattleCityOnline/src/server/wire-spec.js
+++ BattleCityOnline/src/server/wire-spec.js
@@ -10,12 +10,16 @@ define({
                 port: 8283,                
                 express: { $ref: "express" },
                 clientModules: [
-                    { name: "dojo", path: "lib/dojo" },
-                    { name: "dijit", path: "lib/dijit" },
+                    { name: "dojo", path: "amd_modules/dojo" },
+                    { name: "dijit", path: "amd_modules/dijit" },
 
-                    { name: "meld", path: "lib/meld" },
-                    { name: "when", path: "lib/when" },
-                    { name: "wire", path: "lib/wire" },
+                    { name: "box2d-html5", path: "amd_modules/box2d-html5" },
+                    { name: "dcl", path: "amd_modules/dcl" },
+                    { name: "frozen", path: "amd_modules/frozen" },
+
+                    { name: "meld", path: "amd_modules/meld" },
+                    { name: "when", path: "amd_modulesib/when" },
+                    { name: "wire", path: "amd_modules/wire" },
                     
                     { name: "bco-common", path: "src/common" },
                     { name: "bco-client", path: "src/client" }
